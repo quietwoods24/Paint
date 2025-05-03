6d56ab1541bff6e477c0c99b981865e4b09dd146
quietwoods24 <209930033+quietwoods24@users.noreply.github.com>
2025-05-03 22:17:22 +0300

    Add files via upload

---
+++ Forms/Help.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Paint
+{
+    partial class Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Help));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Program is made by quietwoods - ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(218, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "A figure is selected by a single left mouse click on it.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(279, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "A figure is selected by a single left mouse click on it.\nThe figure can be moved " +
+    "by changing the cursor position \nwith the left mouse button pressed.";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(288, 195);
+            this.label4.TabIndex = 6;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 464);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(332, 104);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(13, 143);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(332, 315);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(333, 125);
+            this.panel3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(136, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 31);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Paint";
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 580);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Help";
+            this.Text = "Help";
+            this.Load += new System.EventHandler(this.Help_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Models/Image.cs
@@ -0,0 +1,481 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Added manually for drawing/saving
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+// https://www.newtonsoft.com/json
+using Newtonsoft.Json;
+
+namespace Paint
+{
+
+    public class Image
+    {
+        protected const string delimeter = " | ";
+        protected Pen penFrame;
+        protected List<Shape> shapes;
+
+
+        private Shape selectedShape = null;
+        [JsonIgnore]
+        public Shape SelectedShape
+        {
+            set
+            {
+                if (value == selectedShape)
+                    return;
+
+                // Deselect the previous shape
+                if (selectedShape != null)
+                {
+                    selectedShape.Deselect();
+                    //panelDraw.Cursor = Cursors.Default;
+                }
+
+                // Assign a new shape
+                selectedShape = value;
+
+                // Select a new shape
+                if (selectedShape != null)
+                    selectedShape.Select();
+            }
+
+            get { return selectedShape; }
+        }
+
+        [JsonIgnore]
+        public bool DrawAxes { set; get; } = false;
+
+        public double X { set; get; }
+        public double Y { set; get; }
+        public double Width { set; get; }
+        public double Height { set; get; }
+
+        public Image(double x, double y, double width, double height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            shapes = new List<Shape>();
+
+            // Image frame pen
+            penFrame = new Pen(Color.Black, 1);
+        }
+
+
+        public void Merge(Image image)
+        {
+            if (image == null)
+            {
+                string errorMessage = $"ERROR: Image is null";
+                Console.WriteLine(errorMessage);
+                // throw new Exception(errorMessage);
+                return;
+            }
+
+            Width = Math.Max(Width, image.Width);
+            Height = Math.Max(Height, image.Height);
+
+            SelectedShape = null;
+            if (image != null && image.Count > 0)
+            {
+                for (int i = 0; i < image.Count; i++)
+                    shapes.Add(image[i]);
+            }
+        }
+
+
+        public void Add(Shape shape)
+        {
+            if (shape == null)
+            {
+                string errorMessage = $"ERROR: Shape is null";
+                Console.WriteLine(errorMessage);
+                // throw new Exception(errorMessage);
+                return;
+            }
+
+            SelectedShape = null;
+            if (shape != null && !shapes.Contains(shape))
+                shapes.Add(shape);
+        }
+
+
+        public void Remove(Shape shape)
+        {
+            if (shape == null)
+            {
+                string errorMessage = $"ERROR: Shape is null";
+                Console.WriteLine(errorMessage);
+                // throw new Exception(errorMessage);
+                return;
+            }
+
+            SelectedShape = null;
+            shapes.Remove(shape);
+        }
+
+
+        public void Move(double xOffset, double yOffset)
+        {
+            X += xOffset;
+            Y += yOffset;
+        }
+
+
+        public void MoveInside(double xOffset, double yOffset)
+        {
+            if (SelectedShape != null)
+                SelectedShape.Move(xOffset, yOffset);
+            else
+                foreach (Shape shape in shapes)
+                    shape.Move(xOffset, yOffset);
+        }
+
+
+        public void Zoom(double zoomFactor)
+        {
+            if (zoomFactor <= 0)
+            {
+                string errorMessage = $"ERROR: Zoom factor must be > 0: {zoomFactor}";
+                Console.WriteLine(errorMessage);
+                // throw new ArgumentOutOfRangeException(errorMessage);
+                return;
+            }
+
+            Width *= zoomFactor;
+            Height *= zoomFactor;
+
+            foreach (Shape shape in shapes)
+                shape.Zoom(zoomFactor);
+        }
+
+
+        public void ZoomInside(double zoomFactor)
+        {
+            if (zoomFactor <= 0)
+            {
+                string errorMessage = $"ERROR: Zoom factor must be > 0: {zoomFactor}";
+                Console.WriteLine(errorMessage);
+                // throw new ArgumentOutOfRangeException(errorMessage);
+                return;
+            }
+
+            if (SelectedShape != null)
+                SelectedShape.Zoom(zoomFactor);
+            else
+                foreach (Shape shape in shapes)
+                    shape.Zoom(zoomFactor);
+        }
+
+
+        public void Draw(PaintEventArgs e)
+        {
+            // Draw an image frame
+            Rectangle frame = new Rectangle((int)X - 1, (int)Y - 1, (int)(Width + 1), (int)(Height + 1));
+            e.Graphics.DrawRectangle(penFrame, frame);
+
+            // Draw axes
+            if (DrawAxes)
+            {
+                int dxy = 10;
+                int dz = (int)Math.Min(100, 2 * Y);
+                int xO = (int)X;
+                int yO = (int)Y;
+                int xOX = (int)(X + Width);
+                int yOX = yO;
+                int xOY = xO;
+                int yOY = (int)(Y + Height);
+                int xOZ = (int)(X + Pyramid.XCabinetProjection(new Point3D(0, 0, dz)));
+                int yOZ = (int)(Y + Pyramid.YCabinetProjection(new Point3D(0, 0, dz)));
+                Pen penAxes = new Pen(Color.Black, 3);
+                e.Graphics.DrawLine(penAxes, xO, yO, xOX, yOX);
+                e.Graphics.DrawLine(penAxes, xO, yO, xOY, yOY);
+                e.Graphics.DrawLine(penAxes, xO, yO, xOZ, yOZ);
+
+                // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawstring?view=netframework-4.7.2
+                Font drawFont = new Font("Arial", 10);
+                SolidBrush drawBrush = new SolidBrush(Color.Black);
+                StringFormat drawFormat = new StringFormat();
+                drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+                drawFormat.Alignment = StringAlignment.Center;
+                drawFormat.LineAlignment = StringAlignment.Center;
+                e.Graphics.DrawString("X", drawFont, drawBrush, xOX + dxy, yOX - dxy, drawFormat);
+                e.Graphics.DrawString("Y", drawFont, drawBrush, xOY - dxy, yOY - dxy, drawFormat);
+                e.Graphics.DrawString("Z", drawFont, drawBrush, xOZ - dxy, yOZ, drawFormat);
+            }
+
+            // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.graphics.setclip
+            // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.drawing2d.combinemode
+            // Create an area to crop.
+            frame = new Rectangle((int)X, (int)Y, (int)Width, (int)Height);
+            Region clipRegion = new Region(frame);
+            // Set the graphics cutoff area to region.
+            e.Graphics.SetClip(clipRegion, CombineMode.Replace);
+
+            // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.graphics.translatetransform?view=netframework-4.7.2
+            // Changes the origin of the coordinate system from (0,0) to (X, Y)
+            e.Graphics.TranslateTransform((float)X, (float)Y);
+
+            foreach (Shape shape in shapes)
+                shape.Draw(e);
+        }
+
+
+        // https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.cursor?view=netframework-4.8
+        // https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.cursors?view=netframework-4.8
+        public Cursor MouseMove(object sender, MouseEventArgs e)
+        {
+            bool foundedClicked = false;
+
+            if (e.Button == MouseButtons.Left && SelectedShape != null)
+            {
+                if (SelectedShape.MouseHover(e.X - X, e.Y - Y))
+                    SelectedShape.MouseMoveTo(e.X - X, e.Y - Y);
+            }
+            else if (e.Button == MouseButtons.None)
+            {
+                foreach (Shape shape in shapes)
+                    if (shape.MouseHover(e.X - X, e.Y - Y))
+                    {
+                        foundedClicked = true;
+                        break;
+                    }
+            }
+
+            if (foundedClicked)
+                return Cursors.Hand;
+            else
+                return Cursors.Default;
+        }
+
+
+        public void MouseDown(object sender, MouseEventArgs e)
+        {
+            bool foundedClicked = false;
+            if (e.Button == MouseButtons.Left)
+            {
+                foreach (Shape shape in shapes)
+                {
+                    if (shape.MouseHover(e.X - X, e.Y - Y))
+                    {
+                        // The figure is not yet selected, select it
+                        SelectedShape = shape;
+                        shape.StartMouseMove(e.X - X, e.Y - Y);
+                        foundedClicked = true;
+                        break;
+                    }
+                }
+
+                if (!foundedClicked)
+                    SelectedShape = null;
+            }
+        }
+
+
+        public void MouseUp(object sender, MouseEventArgs e)
+        {
+            if (SelectedShape != null && e.Button == MouseButtons.Left)
+                SelectedShape.StopMouseMove(sender, e);
+        }
+
+
+        public void KeyDown(object sender, KeyEventArgs e, bool insideImage)
+        {
+            const int dXY = 3;
+
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements
+            switch (e.KeyValue)
+            {
+                case 37: // Left
+                    if (insideImage)
+                        MoveInside(-dXY, 0);
+                    else
+                        Move(-dXY, 0);
+                    break;
+
+                case 39: // Right
+                    if (insideImage)
+                        MoveInside(+dXY, 0);
+                    else
+                        Move(+dXY, 0);
+                    break;
+
+                case 38: // Up
+                    if (insideImage)
+                        MoveInside(0, -dXY);
+                    else
+                        Move(0, -dXY);
+                    break;
+
+                case 40: // Down
+                    if (insideImage)
+                        MoveInside(0, +dXY);
+                    else
+                        Move(0, +dXY);
+                    break;
+
+                case 46: // Delete
+                    Remove(SelectedShape);
+                    break;
+
+                case 107: // "+"
+                    if (insideImage)
+                        ZoomInside(1.1);
+                    else
+                        Zoom(1.1);
+                    break;
+
+                case 109: // "-"
+                    if (insideImage)
+                        ZoomInside(0.9);
+                    else
+                        Zoom(0.9);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        public void Save(string filePath)
+        {
+            SelectedShape = null;
+
+            // https://learn.microsoft.com/en-us/dotnet/api/system.io.path.getdirectoryname
+            // https://learn.microsoft.com/en-us/dotnet/api/system.io.directory.exists
+            string directoryName = Path.GetDirectoryName(filePath);
+
+            if (directoryName == "" || Directory.Exists(directoryName))
+            {
+                // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.where?view=netframework-4.8
+                // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.cast?view=netframework-4.8
+                var polygons        = shapes.Where(s => s is Polygon       ).Cast<Polygon>();
+                var filledPolygons  = shapes.Where(s => s is FilledPolygon ).Cast<FilledPolygon>();
+                var regularPolygons = shapes.Where(s => s is RegularPolygon).Cast<RegularPolygon>();
+                var pyramids        = shapes.Where(s => s is Pyramid && !(s is Prism)).Cast<Pyramid>();
+                var prisms          = shapes.Where(s => s is Prism         ).Cast<Prism>();
+
+                // https://www.newtonsoft.com/json/help/html/Overload_Newtonsoft_Json_JsonConvert_SerializeObject.htm
+                // https://www.newtonsoft.com/json/help/html/serializingjson.htm
+                string[] lines =
+                {
+                    JsonConvert.SerializeObject(this),
+                    JsonConvert.SerializeObject(polygons),
+                    JsonConvert.SerializeObject(filledPolygons),
+                    JsonConvert.SerializeObject(regularPolygons),
+                    JsonConvert.SerializeObject(pyramids),
+                    JsonConvert.SerializeObject(prisms)
+                };
+                // https://learn.microsoft.com/en-us/dotnet/api/system.io.file.writealllines
+                File.WriteAllLines(filePath, lines);
+            }
+            else
+            {
+                string errorMessage = $"ERROR: Path does not exist: {directoryName}";
+                Console.WriteLine(errorMessage);
+                // throw new FileNotFoundException(errorMessage);
+            }
+        }
+
+
+        public void Load(string filePath)
+        {
+            SelectedShape = null;
+
+            // https://learn.microsoft.com/en-us/dotnet/api/system.io.file.exists
+            if (File.Exists(filePath))
+            {
+                // In case the file format does not match the required one
+                try
+                {
+                    // https://learn.microsoft.com/en-us/dotnet/api/system.io.file.readlines
+                    var lines = File.ReadAllLines(filePath);
+
+                    // https://www.newtonsoft.com/json/help/html/serializingjson.htm
+                    var jsonIm = JsonConvert.DeserializeObject<Image>(lines[0]);
+                    var jsonPo = JsonConvert.DeserializeObject<List<Polygon>>(lines[1]);
+                    var jsonFP = JsonConvert.DeserializeObject<List<FilledPolygon>>(lines[2]);
+                    var jsonRP = JsonConvert.DeserializeObject<List<RegularPolygon>>(lines[3]);
+                    var jsonPy = JsonConvert.DeserializeObject<List<Pyramid>>(lines[4]);
+                    var jsonPr = JsonConvert.DeserializeObject<List<Prism>>(lines[5]);
+                    if (jsonIm != null)
+                    {
+                        this.X = jsonIm.X;
+                        this.Y = jsonIm.Y;
+                        this.Width = jsonIm.Width;
+                        this.Height = jsonIm.Height;
+                    }
+                    if (jsonPo != null)
+                        shapes.AddRange(jsonPo);
+                    if (jsonFP != null)
+                        shapes.AddRange(jsonFP);
+                    if (jsonRP != null)
+                        shapes.AddRange(jsonRP);
+                    if (jsonPy != null)
+                        shapes.AddRange(jsonPy);
+                    if (jsonPr != null)
+                        shapes.AddRange(jsonPr);
+                }
+                catch (Exception e)
+                {
+                    string errorMessage = $"ERROR: Wrong file format: {filePath}. {e}";
+                    Console.WriteLine(errorMessage);
+                    // throw new Exception(errorMessage);
+                }
+            }
+            else
+            {
+                string errorMessage = $"ERROR: File does not exist: {filePath}";
+                Console.WriteLine(errorMessage);
+                // throw new FileNotFoundException(errorMessage);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            List<string> strShapes = new List<string>();
+            foreach (Shape shape in shapes)
+            {
+                strShapes.Add(shape.ToString());
+            }
+
+            string[] fields =
+            {
+                $"{GetType().Name} ({X:0.###}, {Y:0.###}, {Width:0.###}, {Height:0.###})",
+                $"Shapes: {String.Join(delimeter, strShapes)}"
+            };
+
+            return String.Join(delimeter, fields);
+        }
+
+
+        [JsonIgnore]
+        public double Count { get { return shapes.Count; } }
+
+        [JsonIgnore]
+        public Shape this[int index]
+        {
+            get
+            {
+                if (0 <= index && index < shapes.Count)
+                    return shapes[index];
+                else
+                {
+                    string errorMessage = $"ERROR: Index must be in range [0; {shapes.Count}): {index}";
+                    Console.WriteLine(errorMessage);
+                    // throw new ArgumentOutOfRangeException(errorMessage);
+                    return null;
+                }
+            }
+        }
+    }
+}
