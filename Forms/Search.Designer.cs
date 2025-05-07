fed22905731c14de8c4a1d6d4b155dde477da9b5
quietwoods24 <209930033+quietwoods24@users.noreply.github.com>
2025-05-08 00:14:01 +0300

    Add files via upload
    Changes:
    1) Added correct resizing of the main form;
    2) Attached help form;
    3) Added changing of stroke and fill color for created and selected shape;
    4) Added changing of stroke thickness for created and selected shape;
    5) Added display of current stroke color and fill color;
    6) Added and connected shape search form;
    7) Added shape search by shape name, fill and stroke color;
    8) Added program icon.

---
+++ Forms/Search.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Paint
+{
+    partial class Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StrFillBox = new System.Windows.Forms.TextBox();
+            this.StrColBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.button_Find = new System.Windows.Forms.Button();
+            this.listBox_search = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.StrFillBox);
+            this.panel1.Controls.Add(this.StrColBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.NameBox);
+            this.panel1.Controls.Add(this.button_Find);
+            this.panel1.Location = new System.Drawing.Point(42, 38);
+            this.panel1.MinimumSize = new System.Drawing.Size(420, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(420, 141);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Shape fill color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Shape stroke color";
+            // 
+            // StrFillBox
+            // 
+            this.StrFillBox.Location = new System.Drawing.Point(121, 71);
+            this.StrFillBox.Name = "StrFillBox";
+            this.StrFillBox.Size = new System.Drawing.Size(270, 20);
+            this.StrFillBox.TabIndex = 4;
+            // 
+            // StrColBox
+            // 
+            this.StrColBox.Location = new System.Drawing.Point(121, 45);
+            this.StrColBox.Name = "StrColBox";
+            this.StrColBox.Size = new System.Drawing.Size(270, 20);
+            this.StrColBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Shape name";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(121, 19);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(270, 20);
+            this.NameBox.TabIndex = 1;
+            // 
+            // button_Find
+            // 
+            this.button_Find.Location = new System.Drawing.Point(165, 97);
+            this.button_Find.Name = "button_Find";
+            this.button_Find.Size = new System.Drawing.Size(85, 28);
+            this.button_Find.TabIndex = 0;
+            this.button_Find.Text = "Find";
+            this.button_Find.UseVisualStyleBackColor = true;
+            this.button_Find.Click += new System.EventHandler(this.button_Find_Click);
+            // 
+            // listBox_search
+            // 
+            this.listBox_search.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_search.FormattingEnabled = true;
+            this.listBox_search.Location = new System.Drawing.Point(12, 200);
+            this.listBox_search.Name = "listBox_search";
+            this.listBox_search.Size = new System.Drawing.Size(484, 199);
+            this.listBox_search.TabIndex = 1;
+            // 
+            // Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 411);
+            this.Controls.Add(this.listBox_search);
+            this.Controls.Add(this.panel1);
+            this.Name = "Search";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.Search_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox StrFillBox;
+        private System.Windows.Forms.TextBox StrColBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button button_Find;
+        private System.Windows.Forms.ListBox listBox_search;
+    }
+}
---
+++ Models/Shape3D.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Added manually for Drawing
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Paint
+{
+
+    public class Pyramid : Shape2D
+    {
+        public Point3D Vertex { set; get; }
+
+        public Pyramid(Point3D vertex, params Point2D[] points) : base(points)
+        {
+            Vertex = vertex;
+        }
+
+
+        // 2D Movement in the XY plane
+        public override void Move(double xOffset, double yOffset)
+        {
+            // Move the base of the pyramid
+            base.Move(xOffset, yOffset);
+
+            // Move the top of the pyramid
+            Vertex.X += xOffset;
+            Vertex.Y += yOffset;
+        }
+
+
+        // 3D Zoom
+        public override void Zoom(double zoomFactor)
+        {
+            if (zoomFactor <= 0)
+            {
+                string errorMessage = $"ERROR: Zoom factor must be > 0: {zoomFactor}";
+                Console.WriteLine(errorMessage);
+                // throw new ArgumentOutOfRangeException(errorMessage);
+                return;
+            }
+
+            // Pyramid base re-design
+            base.Zoom(zoomFactor);
+
+            // Pyramid top re-size
+            Vertex.X *= zoomFactor;
+            Vertex.Y *= zoomFactor;
+            Vertex.Z *= zoomFactor;
+        }
+
+        /*
+        https://en.wikipedia.org/wiki/Oblique_projection#Cabinet_projection
+        Oblique frontal dimensional projection 
+        
+
+                          ^ Y 
+                          | 
+                          | 
+                          | 
+                        y +-----------+             
+                         /|          /|
+                        / |         / |
+                       /_________  / P(x,y,z)
+                      |   |       *   |  
+                      |   o-------|---+---------> X
+                      |  /        |  / x
+                      | /         | /
+                      |/__________|/
+                    z /               X = x + 0.5 * z * cos(225)
+                     /                Y = y + 0.5 * z * sin(225)
+                    /
+                   Z
+                  
+
+                                      x                  
+                          o-----------+---------> X
+                         /|           | 
+                        / |           |
+                     z /  |y          |
+                      /|  +-----------+       
+                     / | /|          /        
+                    /  |/ |         /   X = x + 0.5 * z * cos(225)
+                   /   +-----------*    Y = y - 0.5 * z * sin(225)  // "-" because OY is pointing downward.
+                  /       |      P(x,y,z)       
+                 /        |
+                /         |
+               Z          Y
+
+
+                                 Z           
+                                /
+                               /            
+                            z +-----------+
+                             /|          /|
+                            / |         / |
+                           /  |      x /  |                
+                          o———————————+—————————————> X
+                          |   |_ _ _ _|_ _| 
+                          |  /        |  / P(x,y,z)
+                          | /         | /
+                          |/          |/
+                        y +———————————+       
+                          |          
+                          |            X = x + 0.5 * z * cos(-45)
+                          Y            Y = y + 0.5 * z * sin(-45)
+                           
+                  
+        */
+        public static double XCabinetProjection(Point3D point)
+        {
+            return point.X + 0.5 * point.Z * Math.Cos(-Math.PI * 1 / 4);
+        }
+
+
+        public static double YCabinetProjection(Point3D point)
+        {
+            return point.Y + 0.5 * point.Z * Math.Sin(-Math.PI * 1 / 4);
+        }
+
+
+        public override void Draw(PaintEventArgs e)
+        {
+            if (points.Length <= 0)
+                return;
+
+            // Drawing the base of the pyramid
+            base.Draw(e);
+
+
+            // Drawing the edges of a pyramid
+            // Create pen
+            Pen pen = new Pen(StrokeColor, 1); // StrokeWidth
+            Pen penSelected = new Pen(selectedColor, selectedWidth);
+
+            float vertexX = (float)XCabinetProjection(Vertex);
+            float vertexY = (float)YCabinetProjection(Vertex);
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawline?view=netframework-4.7.2
+                e.Graphics.DrawLine(selected ? penSelected : pen,
+                    vertexX, vertexY,
+                    (float)points[i].X, (float)points[i].Y);
+            }
+
+            // Draw a vertex marker
+            e.Graphics.DrawEllipse(selected ? penSelected : pen, (int)vertexX - markerSize, (int)vertexY - markerSize, 2 * markerSize, 2 * markerSize);
+        }
+
+
+        public override double GetPerimeter()
+        {
+            // Perimeter of the base of the pyramid
+            //double result = (new Shape2D(points)).GetPerimeter();
+            double result = base.GetPerimeter();
+
+            // Lengths of the edges of the pyramid
+            for (int i = 0; i < points.Length; i++)
+            {
+                result += Distance(Vertex, points[i]);
+            }
+            return result;
+        }
+
+
+        public override double GetArea()
+        {
+            // Area of the base of the pyramid
+            //double result = (new Shape2D(points)).GetArea();
+            double result = base.GetArea();
+
+            // Area of the side faces of the pyramid
+            for (int i = 0; i < points.Length; i++)
+            {
+                // (i+1) % N - for the case of a loop => the first point is connected to the last
+                int j = (i + 1) % points.Length;
+                double a = Distance(Vertex, points[i]);
+                double b = Distance(Vertex, points[j]);
+                double c = Distance(points[i], points[j]);
+                result += TriangleArea(a, b, c);
+            }
+            return result;
+        }
+
+
+        public override string ToString()
+        {
+            string[] fields =
+            {
+                base.ToString(),
+                $"Vertex: {Vertex.ToString()}"
+            };
+            return String.Join(delimeter, fields);
+        }
+    }
+
+
+    public class Prism : Pyramid
+    {
+        public Prism(Point3D vertex, params Point2D[] points) : base(vertex, points)
+        {
+            // Do nothing
+        }
+
+
+        public override void Draw(PaintEventArgs e)
+        {
+            if (points.Length <= 0)
+                return;
+
+            // Drawing the bases of the prism
+            PointF[] upperBasePoints = new PointF[points.Length];
+            PointF[] lowerBasePoints = new PointF[points.Length];
+
+            // Shift of the upper base relative to the lower base
+            double dX = Vertex.X - points[0].X;
+            double dY = Vertex.Y - points[0].Y;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                // Copy the array of points Point2D to the array of points PointF
+                lowerBasePoints[i] = new PointF((float)points[i].X, (float)points[i].Y);
+
+                var iVertex = new Point3D(points[i].X + dX, points[i].Y + dY, Vertex.Z);
+                upperBasePoints[i] = new PointF((float)XCabinetProjection(iVertex), (float)YCabinetProjection(iVertex));
+            }
+
+            // Create a pen
+            Pen pen = new Pen(StrokeColor, (float)StrokeWidth);
+            Pen penSelected = new Pen(selectedColor, selectedWidth);
+
+            // Drawing the lower base of the prism
+            e.Graphics.DrawPolygon(selected ? penSelected : pen, lowerBasePoints);
+
+            pen.Width = 1;
+            // Drawing the lower base of the prism
+            e.Graphics.DrawPolygon(selected ? penSelected : pen, upperBasePoints);
+
+            // Drawing the edges of a prism
+            for (int i = 0; i < points.Length; i++)
+            {
+                // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawline?view=netframework-4.7.2
+                e.Graphics.DrawLine(selected ? penSelected : pen, lowerBasePoints[i], upperBasePoints[i]);
+
+                // Draw the bottom base markers
+                // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawellipse?view=netframework-4.8
+                e.Graphics.DrawEllipse(selected ? penSelected : pen,
+                    (int)lowerBasePoints[i].X - markerSize, (int)lowerBasePoints[i].Y - markerSize, 2 * markerSize, 2 * markerSize);
+            }
+            // Draw a vertex marker
+            e.Graphics.DrawEllipse(selected ? penSelected : pen,
+                (int)upperBasePoints[0].X - markerSize, (int)upperBasePoints[0].Y - markerSize, 2 * markerSize, 2 * markerSize);
+        }
+
+
+        public override double GetPerimeter()
+        {
+            // Perimeter of the prism bases
+            double result = 2 * (new Shape2D(points)).GetPerimeter();
+            // double result = 2 * ((Shape2D)this).GetPerimeter(); // NOT WORKING
+
+            // Lengths of the prism edges
+            result += points.Length * Distance(Vertex, points[0]);
+
+            return result;
+        }
+
+
+        public override double GetArea()
+        {
+            // Area of the prism bases
+            double result = 2 * (new Shape2D(points)).GetArea();
+
+            // Area of prism bases // Area of prism side faces
+            // Offset of the top face relative to the base face (in 3D)
+            double dX = Vertex.X - points[0].X;
+            double dY = Vertex.Y - points[0].Y;
+            for (int i = 0; i < points.Length; i++)
+            {
+                // (i+1) % N - for the case of a short circuit => the first point is connected to the last
+                int j = (i + 1) % points.Length;
+                double a = Distance(points[i], points[j]);
+                double b = Distance(Vertex, points[0]); // the length of the side edges is the same, can be removed from the cycle
+                Point3D vertexPj = new Point3D(points[j].X + dX, points[j].Y + dY, Vertex.Z);
+                double d = Distance(points[i], vertexPj); // diagonal of the face
+                result += 2 * TriangleArea(a, b, d);
+            }
+            return result;
+        }
+    }
+}
+
